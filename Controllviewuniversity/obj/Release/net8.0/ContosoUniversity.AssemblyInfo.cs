2055408a484c015f5476a53ec80b391732435587
Opilane TTHK <opilane@tthk.ee>
2024-09-27 10:03:16 +0300

    Department edit pooleli

--- Controllviewuniversity/obj/Release/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ Controllviewuniversity/obj/Release/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+275ae447b9365253062e2b20e5c26b98c6346bd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a898ba2877f940d114c6d9dff29f9ab3aee10d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
