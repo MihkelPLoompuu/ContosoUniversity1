33f0bb011f76dfbcb06a753b18bf4008d1605a0d
Opilane TTHK <opilane@tthk.ee>
2024-10-03 10:52:38 +0300

    ns

--- Controllviewuniversity/Controllers/CoursesController.cs
+++ Controllviewuniversity/Controllers/CoursesController.cs
@@ -6,14 +6,6 @@ namespace ContosoUniversity.Controllers
 {
     public class CoursesController : Controller
     {
-        private readonly SchoolContext _context;
-        public CoursesController(SchoolContext context)
-        {
-            _context = context;
-        }
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Courses.ToListAsync());
-        }
+
     }
 }
