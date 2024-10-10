565cdd6e3a4692d7b6854e94cd816258eea81144
Opilane TTHK <opilane@tthk.ee>
2024-10-10 14:00:42 +0300

    In delete

--- Controllviewuniversity/Controllers/InstructorsController.cs
+++ Controllviewuniversity/Controllers/InstructorsController.cs
@@ -84,8 +84,36 @@ namespace ContosoUniversity.Controllers
             PopulateAssignedCourseData(instructor); //uuendab instructori juures olevaid kursuseid
             return View(instructor);
         }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-       
+            var instructor = await _context.Instructors
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+
+            return View(instructor);
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var instructor = await _context.Instructors.FindAsync(id); //ostsime andmebaasist õpilast id järgi ja paneme ta students nimelisse objekti voi muutujasse
+            _context.Instructors.Remove(instructor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> Edit(int? id)
         {
