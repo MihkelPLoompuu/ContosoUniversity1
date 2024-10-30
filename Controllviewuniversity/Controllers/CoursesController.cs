c7150009deb86686f731c507d8e32074085e8d88
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2024-10-30 10:18:49 +0200

    ns

--- Controllviewuniversity/Controllers/CoursesController.cs
+++ Controllviewuniversity/Controllers/CoursesController.cs
@@ -17,39 +17,7 @@ namespace ContosoUniversity.Controllers
         {
             return View(await _context.Courses.ToListAsync());
         }
-        public async Task<IActionResult> DetailsDelete(int? id, string actionType)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var course = await _context.Courses.FirstOrDefaultAsync(m => m.CourseID == id);
-            if (course == null)
-            {
-                return NotFound();
-            }
-            ViewData["actionType"] = actionType ?? "Details";
-            return View(course);
-        }        
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DetailsDeleteConfirmed(int id, string actionType, Course course)
-        {
-            if (actionType == "Details")
-            {
-                _context.Courses.Add(course);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                _context.Courses.Remove(course);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }            
-        }
 
         [HttpGet]
         public async Task<IActionResult> CreateEdit(int? id, string actionType)
